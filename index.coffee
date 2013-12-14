9efa5b976336ff216ed87f568d2c7e44bf6df27f
deathcap <deathcap@gmx.co.uk>
2013-12-14 10:18:14 -0800

    Move ItemPile to its own module
    https://github.com/deathcap/itempile

--- index.coffee
+++ index.coffee
@@ -1,7 +1,9 @@
 # vim: set shiftwidth=2 tabstop=2 softtabstop=2 expandtab:
 
 deepEqual = require 'deep-equal'
+ItemPile = require 'itempile'
 
+module.exports =
 class Inventory
   constructor: (size, opts) ->
     size = size ? 10
@@ -57,105 +59,3 @@ class Inventory
   slot: (i) ->
     @array[i]
 
-class ItemPile
-
-  constructor: (item, count, tags) ->
-    @item = if typeof(item) == 'string' then ItemPile.itemFromString(item) else item
-    @count = count ? 1
-    @tags = tags ? {}
-
-  # maximum size items should pile to
-  @maxPileSize = 64
-
-  # convert item<->string; change these to use non-string items
-  @itemFromString: (s) ->
-    if s instanceof ItemPile then return s
-    if !s then '' else s
-
-  @itemToString: (item) ->
-    ''+item
-
-  hasTags: () ->
-    Object.keys(@tags).length != 0    # not "{}"
-
-  matchesType: (itemPile) ->
-    @item == itemPile.item
-
-  matchesTypeAndCount: (itemPile) ->
-    @item == itemPile.item && @count == itemPile.count
-
-  matchesTypeAndTags: (itemPile) ->
-    @item == itemPile.item && deepEqual(@tags, itemPile.tags, {strict:true})
-
-  matchesAll: (itemPile) ->
-    @matchesTypeAndCount(itemPile) && deepEqual(@tags, itemPile.tags, {strict:true})
-
-  # can this pile be merged with another?
-  canPileWith: (itemPile) ->
-    return false if itemPile.item != @item
-    return false if itemPile.hasTags() or @hasTags() # any tag data makes unpileable
-    true
-
-  # combine two piles if possible, altering both this and argument pile
-  # returns count of items that didn't fit
-  mergePile: (itemPile) ->
-    return false if not @canPileWith(itemPile)
-    itemPile.count = @increase(itemPile.count)
-
-  # increase count by argument, returning number of items that didn't fit
-  increase: (n) ->
-    [newCount, excessCount] = @tryAdding(n)
-    @count = newCount
-    return excessCount
-
-  # decrease count by argument, returning number of items removed
-  decrease: (n) ->
-    [removedCount, remainingCount] = @trySubtracting(n)
-    @count = remainingCount
-    return removedCount
-
-  # try combining count of items up to max pile size, returns [newCount, excessCount]
-  tryAdding: (n) ->
-    sum = @count + n
-    if sum > ItemPile.maxPileSize
-      return [ItemPile.maxPileSize, sum - ItemPile.maxPileSize] # overflowing pile
-    else
-      return [sum, 0] # added everything they wanted
-
-  # try removing count of items, returns [removedCount, remainingCount]
-  trySubtracting: (n) ->
-    difference = @count - n
-    if difference < 0
-      return [@count, n - @count] # didn't have enough
-    else
-      return [n, @count - n]  # had enough, some remain
-
-  # remove count of argument items, returning new pile of those items which were split off
-  splitPile: (n) ->
-    return false if n > @count
-    @count -= n
-
-    return new ItemPile(@item, n, @tags)
-
-  toString: () ->
-    if @hasTags()
-      "#{@count}:#{@item} #{JSON.stringify @tags}"
-    else
-      "#{@count}:#{@item}"
-
-  @fromString: (s) ->
-    a = s.match(/^([^:]+):([^ ]+) ?(.*)/) # assumptions: positive integral count, item name no spaces
-    return undefined if not a
-    [_, countStr, itemStr, tagsStr] = a
-    count = parseInt(countStr, 10)
-    item = ItemPile.itemFromString(itemStr)
-    if tagsStr && tagsStr.length
-      tags = JSON.parse(tagsStr)
-    else
-      tags = {}
-
-    return new ItemPile(item, count, tags)
-
-
-module.exports.Inventory = Inventory
-module.exports.ItemPile = ItemPile
--- index.js
+++ index.js
@@ -4,7 +4,9 @@
 
   deepEqual = require('deep-equal');
 
-  Inventory = (function() {
+  ItemPile = require('itempile');
+
+  module.exports = Inventory = (function() {
     function Inventory(size, opts) {
       size = size != null ? size : 10;
       this.array = new Array(size);
@@ -95,145 +97,5 @@
     return Inventory;
 
   })();
-
-  ItemPile = (function() {
-    function ItemPile(item, count, tags) {
-      this.item = typeof item === 'string' ? ItemPile.itemFromString(item) : item;
-      this.count = count != null ? count : 1;
-      this.tags = tags != null ? tags : {};
-    }
-
-    ItemPile.maxPileSize = 64;
-
-    ItemPile.itemFromString = function(s) {
-      if (s instanceof ItemPile) {
-        return s;
-      }
-      if (!s) {
-        return '';
-      } else {
-        return s;
-      }
-    };
-
-    ItemPile.itemToString = function(item) {
-      return '' + item;
-    };
-
-    ItemPile.prototype.hasTags = function() {
-      return Object.keys(this.tags).length !== 0;
-    };
-
-    ItemPile.prototype.matchesType = function(itemPile) {
-      return this.item === itemPile.item;
-    };
-
-    ItemPile.prototype.matchesTypeAndCount = function(itemPile) {
-      return this.item === itemPile.item && this.count === itemPile.count;
-    };
-
-    ItemPile.prototype.matchesTypeAndTags = function(itemPile) {
-      return this.item === itemPile.item && deepEqual(this.tags, itemPile.tags, {
-        strict: true
-      });
-    };
-
-    ItemPile.prototype.matchesAll = function(itemPile) {
-      return this.matchesTypeAndCount(itemPile) && deepEqual(this.tags, itemPile.tags, {
-        strict: true
-      });
-    };
-
-    ItemPile.prototype.canPileWith = function(itemPile) {
-      if (itemPile.item !== this.item) {
-        return false;
-      }
-      if (itemPile.hasTags() || this.hasTags()) {
-        return false;
-      }
-      return true;
-    };
-
-    ItemPile.prototype.mergePile = function(itemPile) {
-      if (!this.canPileWith(itemPile)) {
-        return false;
-      }
-      return itemPile.count = this.increase(itemPile.count);
-    };
-
-    ItemPile.prototype.increase = function(n) {
-      var excessCount, newCount, _ref;
-      _ref = this.tryAdding(n), newCount = _ref[0], excessCount = _ref[1];
-      this.count = newCount;
-      return excessCount;
-    };
-
-    ItemPile.prototype.decrease = function(n) {
-      var remainingCount, removedCount, _ref;
-      _ref = this.trySubtracting(n), removedCount = _ref[0], remainingCount = _ref[1];
-      this.count = remainingCount;
-      return removedCount;
-    };
-
-    ItemPile.prototype.tryAdding = function(n) {
-      var sum;
-      sum = this.count + n;
-      if (sum > ItemPile.maxPileSize) {
-        return [ItemPile.maxPileSize, sum - ItemPile.maxPileSize];
-      } else {
-        return [sum, 0];
-      }
-    };
-
-    ItemPile.prototype.trySubtracting = function(n) {
-      var difference;
-      difference = this.count - n;
-      if (difference < 0) {
-        return [this.count, n - this.count];
-      } else {
-        return [n, this.count - n];
-      }
-    };
-
-    ItemPile.prototype.splitPile = function(n) {
-      if (n > this.count) {
-        return false;
-      }
-      this.count -= n;
-      return new ItemPile(this.item, n, this.tags);
-    };
-
-    ItemPile.prototype.toString = function() {
-      if (this.hasTags()) {
-        return "" + this.count + ":" + this.item + " " + (JSON.stringify(this.tags));
-      } else {
-        return "" + this.count + ":" + this.item;
-      }
-    };
-
-    ItemPile.fromString = function(s) {
-      var a, count, countStr, item, itemStr, tags, tagsStr, _;
-      a = s.match(/^([^:]+):([^ ]+) ?(.*)/);
-      if (!a) {
-        return void 0;
-      }
-      _ = a[0], countStr = a[1], itemStr = a[2], tagsStr = a[3];
-      count = parseInt(countStr, 10);
-      item = ItemPile.itemFromString(itemStr);
-      if (tagsStr && tagsStr.length) {
-        tags = JSON.parse(tagsStr);
-      } else {
-        tags = {};
-      }
-      return new ItemPile(item, count, tags);
-    };
-
-    return ItemPile;
-
-  })();
-
-  module.exports.Inventory = Inventory;
-
-  module.exports.ItemPile = ItemPile;
 
 }).call(this);
